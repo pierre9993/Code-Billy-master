c89b14788b49965362e7b02386b2289cb8ca861e
Morle Lucas <m.pierre9993@gmail.com>
2020-11-19 17:15:46 +0100

    1

--- Javas/exo objet/script/script.js
+++ Javas/exo objet/script/script.js
@@ -2,53 +2,70 @@
  * Déclaration de la variable contenant les informations sur les élèves
  */
 
-var info_eleves = [
-    {nom: "Dupont",prenom: "Valérie" },
-    {nom: "Boha",prenom: "Billy"},
-    {nom: "Mossan",prenom: "Bastien"},
-    {nom: "Cruise",prenom: "Ines"},
+var eleves = [
+    { nom: "Dupont", prenom: "Valérie", age: "30 ans" },
+    { nom: "Boha", prenom: "Billy", age: "25 ans" },
+    { nom: "Mossan", prenom: "Bastien", age: "36 ans" },
+    { nom: "Cruise", prenom: "Ines", age: "42 ans" },
 ];
+// execute la fonction qui affiche les élèves automatiquements au lancement
+
 /**
  * Fonction qui affiche le nom et le prénom des élèves sous forme de liste
  */
-function afficherEleves() {
-    document.getElementById("elelist").innerHTML = "";
-    //Reset du contenu de la liste
+function afficherEleves(eleves) {
+   videTableau();   //Reset du contenu de la liste
 
-    for (i = 0; i < info_eleves.length; i++) {
+    for (i = 0; i < eleves.length; i++) {
         //boucle qui affiche autant d'élèves qu'il y en a dans la variable info_eleves
 
-        var eleves_objet = info_eleves[i];
-        document.getElementById("elelist").innerHTML += "<li><hr/><br/> <b><u>Nom</u></b> : " + eleves_objet.nom + "<b> <u> Prénom</u></b> : " + eleves_objet.prenom + "</li><br/>"
+        var eleves_objet = eleves[i];
+        document.getElementById("elelist").innerHTML += "<li><hr/><br/> <b><u>Nom</u></b> : " + eleves_objet.nom + "<b> <u> Prénom</u></b> : " + eleves_objet.prenom + "<b> <u > Âge </u></b> : " + eleves_objet.age + "</li><br/>";
+        document.getElementById("eletable").innerHTML += "<tr> <td>" + eleves_objet.nom + "</td> <td>" + eleves_objet.prenom + " </td> <td>" + eleves_objet.age + "</td>  </tr>";
     }
 }
-/**
- * Fonction qui affiche uniquement le nom des élèves sous forme de liste
- */
 
-function afficherNomEleves() {
+function videTableau(){
     document.getElementById("elelist").innerHTML = "";
-//Reset du contenu de la liste
-    for (i = 0; i < info_eleves.length; i++) {
-        var eleves_objet = info_eleves[i];
-        eleves_objet.sort(function(a, b){return a - b});
-
-        document.getElementById("elelist").innerHTML += "<li> <b><u>Nom</u> :</b> " + eleves_objet.nom + "</li><br/><hr/>"
-    }
-}
-/**
- * Fonction qui affiche uniquement le prénom des élèves sous forme de liste
- */
-function afficherPrenomEleves() {
-    document.getElementById("elelist").innerHTML = "";
-//Reset du contenu de la liste
-
-    for (i = 0; i < info_eleves.length; i++) {
-        var eleves_objet = info_eleves[i];
-        eleves_objet.prenom.sort();
-
-        document.getElementById("elelist").innerHTML += "<li> <b><u>Prénom</u> :</b> " + eleves_objet.prenom + "</li><br/><hr/>"
-    }
+    document.getElementById("eletable").innerHTML = "<thead><tr>    <th>Nom <button onclick='triEleves(eleves,'nom')'>trier</button></th>    <th>Prenom <button onclick='triEleves(eleves,'prenom')'>trier</button></th>  <th>Âge<button onclick='triEleves(eleves,'age')'>trier</button></th></tr></thead>";
 }
 
+
+
+
+var bascules = {"nom" : null, "prenom" : null, "age": null };
+
+function trieEleve(eleves, propriete) {
+    console.log(eleves)
+    // pour trier, je dois pouvoir comparer les éléments de la liste entre eux !
+    // ex : [2, 5, 1]  : 1 < 2 < 5
+    function comparerParPropriete (eleve1, eleve2) {
+      return comparerEleve(eleve1, eleve2, propriete);
+    }
+    
+    eleves.sort(comparerParPropriete);
+    
+    videTableau();
+    afficherEleves(eleves);
+  }
+  
+  /**
+   * Fonction qui permet de comparer 2 objets "élève"
+   *
+   * @param {Object} eleve1
+   * @param {Object} eleve2
+   * @param {string} propriete Propriété sur laquelle on trie
+   * @returns {number} 1 si eleve1 > eleve2, -1 sinon
+   */
+  function comparerEleve(eleve1, eleve2, propriete) {
+    if (bascules[propriete] === null || bascules[propriete] === true){
+      var greater = eleve1[propriete] > eleve2[propriete] ? 1 : -1;
+      bascules[propriete] = false;
+    }
+    else {
+      var greater = eleve1[propriete] < eleve2[propriete] ? 1 : -1;
+      bascules[propriete] = true;
+    }
+    return greater;
+  }
   
